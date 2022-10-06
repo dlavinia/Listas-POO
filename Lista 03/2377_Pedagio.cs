6553913852697e86912c92fe96c3b9a801057027
Débora Melo <56905620+dlavinia@users.noreply.github.com>
2022-10-06 13:27:58 +0000

    Pedagio e Distancia entre dois pontos

--- Lista 03/2377_Pedagio.cs
+++ Lista 03/2377_Pedagio.cs
@@ -1,1 +1,24 @@
-#teste
+
+using System;
+
+class Program {
+  public static void Main() {
+    int l, d, k, p, custo;
+    string l1 = Console.ReadLine();
+    string l2 = Console.ReadLine();
+
+    string[] x = l1.Split();
+    string[] y = l2.Split();
+
+    l = int.Parse(x[0]);
+    d = int.Parse(x[1]);
+
+    k = int.Parse(y[0]);
+    p = int.Parse(y[1]);
+    
+    custo = (l * k) + ((l / d) * p);
+
+    Console.WriteLine(custo);
+
+  }
+}
