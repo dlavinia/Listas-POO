8b03475230bfca6fd17f4a8b0f074423ceea2790
Débora Melo <56905620+dlavinia@users.noreply.github.com>
2022-10-20 12:44:53 +0000

    Maior

--- Lista07/Maior.cs
+++ Lista07/Maior.cs
@@ -2,7 +2,18 @@ using System;
 
 class Program {
   public static void Main() {
-    string s = Console.ReadLine();                                                                            
-    Console.WriteLine($"{s}");
+    double a = double.Parse(Console.ReadLine());        
+    double b = double.Parse(Console.ReadLine());    
+    Console.WriteLine(Maior(a, b));
+  }
+  
+   public static double Maior(double a, double b){
+     double maior;
+    if (a > b){
+      maior = a;
+    } else{
+      maior = b;
+    }
+    return maior;
   }
 }
