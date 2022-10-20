32b8c64b15763bfc88cd48763869a58b9235ce10
Débora Melo <56905620+dlavinia@users.noreply.github.com>
2022-10-20 12:36:41 +0000

    Formatar Texto

--- Lista07/Maior.cs
+++ Lista07/Maior.cs
@@ -1,1 +1,8 @@
-#Maior
+using System;
+
+class Program {
+  public static void Main() {
+    string s = Console.ReadLine();                                                                            
+    Console.WriteLine($"{s}");
+  }
+}
